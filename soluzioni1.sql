f3fa0ab9160088ccf40f87d046a024838e78bc21
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-04 15:17:46 +0200

    ADD es 2

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -18,3 +18,10 @@ SELECT *
 FROM `students`
 WHERE `students`.`date_of_birth` BETWEEN '1990-01-01' AND '1990-12-31';
 
+/*
+2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+*/
+SELECT *
+FROM `courses`
+WHERE `courses`.`cfu` > 10;
+
