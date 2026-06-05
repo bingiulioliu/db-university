034e11444f40907713922dd3510ec8d02b24a2a5
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-05 15:42:44 +0200

    ADD es 2 group by

--- soluzioniGroupBy.sql
+++ soluzioniGroupBy.sql
@@ -7,3 +7,12 @@
 select office_address, count(*) as 'numero_insegnanti'
 from teachers t
 group by office_address;
+
+/*
+2. Contare quanti iscritti ci sono stati ogni anno
+*/
+select year(enrolment_date) as anno_iscrizione, count(*) as enrolments
+from students s
+group by year(enrolment_date)
+order by anno_iscrizione
+
