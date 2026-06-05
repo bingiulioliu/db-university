6020e5f11705dd8da8237baedb67a99347844c52
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-05 16:14:41 +0200

    ADD es 2 join

--- soluzioniJoin.sql
+++ soluzioniJoin.sql
@@ -11,4 +11,13 @@ from students as s
 		on d.id = s.degree_id
 having d.name = 'Corso di Laurea in Economia';
 
-
+/*
+2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di
+Neuroscienze
+*/
+select *
+from departments as dep
+	join degrees as deg
+		on dep.id = deg.department_id
+having dep.name = 'Dipartimento di Neuroscienze'
+	and deg.level = 'magistrale'
